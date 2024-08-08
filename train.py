a0a32a50f045d0cf864249566994c984b2e0869c
JiwooDady <43941290+JiwooDady@users.noreply.github.com>
2024-08-08 06:10:11 +0000

    init

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ warnings.filterwarnings(action="ignore")
 
 
 if __name__ == "__main__":
-    train_df = pd.read_csv(os.path.join(DATA_PATH, "house_rent_train.csv"))
+    train_df = pd.read_csv(os.path.join(DATA_PATH, "bike_sharing_train.csv"))
     logger.debug("Load data...")
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
